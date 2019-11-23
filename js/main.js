bdb127e9290cef61d5f0092b6b1e8da15459fc70
unknown <jet333@yandex.ru>
2019-11-23 22:09:39 +0300

    Верстаем секцию со скелетом

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(function() {
   /* реализация с табами tabs */
-  $('.list__inner .tab').on('click', function(event) {
-  var id = $(this).attr('data-id');
-  $('.list__inner').find('.tab-item').removeClass('active-tab').hide();
-  $('.list__inner .tabs').find('.tab').removeClass('active');
-  $(this).addClass('active');
-  $('#'+id).addClass('active-tab').fadeIn();
+  $('.list__inner .tab').on('click', function(event) { //при наведении тоже самое,только вместо "on" пишем "hover" и убираем "click"
+  var id = $(this).attr('data-id');// записываем в переменную id аттрибут элемента ,на которого кликнули
+  $('.list__inner').find('.tab-item').removeClass('active-tab').hide()//у родителя... находим элемент с классом... и удаляем у него класс active и скрываем элемент
+  $('.list__inner .tabs').find('.tab').removeClass('active');//...удаляем класс active ... 
+  $(this).addClass('active');// добавляем класс active у кликнутого элемента
+  $('#'+id).addClass('active-tab').fadeIn();//находим элемент с id(#+id), добавляем ему класс... и показываем его
   return false;
 });
 
