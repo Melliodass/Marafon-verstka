90aefdcb240b4fd9c6bd10f3ff20212d4e9217d1
unknown <jet333@yandex.ru>
2020-02-27 00:30:16 +0300

    секция rewiws

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 $(function() {
   /* реализация с табами tabs */
-  $('.list__inner .tab').on('click', function(event) { //при наведении тоже самое,только вместо "on" пишем "hover" и убираем "click"
+  $('.list__inner .tab, .rewiews__tab').on('click', function(event) { //при наведении тоже самое,только вместо "on" пишем "hover" и убираем "click"
   var id = $(this).attr('data-id');// записываем в переменную id аттрибут элемента ,на которого кликнули
-  $('.list__inner').find('.tab-item').removeClass('active-tab').hide()//у родителя... находим элемент с классом... и удаляем у него класс active и скрываем элемент
-  $('.list__inner .tabs').find('.tab').removeClass('active');//...удаляем класс active ... 
+    $('.list__inner, .rewiews__inner').find('.tab-item, .rewiews__content-item ').removeClass('active-tab').hide()//у родителя... находим элемент с классом... и удаляем у него класс active и скрываем элемент
+    $('.list__inner .tabs, .rewiews__tabs ').find('.tab, .rewiews__tab').removeClass('active');//...удаляем класс active ... 
   $(this).addClass('active');// добавляем класс active у кликнутого элемента
   $('#'+id).addClass('active-tab').fadeIn();//находим элемент с id(#+id), добавляем ему класс... и показываем его
   return false;
 });
-
+ 
 });
 
 
